a90e62937c5b56e40a91a80a7f1b69b7256c3362
Артемий <fe77.dev@gmail.com>
2024-05-19 19:04:09 +0300

    feat(prettier.config.js): prettier stings

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  bracketSpacing: false,
+  htmlWhitespaceSensitivity: "ignore",
+  ignorePatterns: ["node_modules/", "dist/", "*.min.js"],
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  spaceBeforeFunctionParen: true,
+};
